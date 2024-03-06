68f899d9b7983ced4587de5a761c877ca478512a
Rohit-saxena125 <rohitsaxena125@gmail.com>
2024-03-06 14:45:41 +0530

    product done

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import LoginForm from "./Component/loginform";
 import Signupform from "./Component/Signupform";
 import Landing from "./Component/landing";
 import Error from "./Component/error";
+import Logout from "./Component/logout";
+import Product from "./Component/Product";
 function App() {
   return (
     <>
@@ -11,6 +13,8 @@ function App() {
       <Route exact path="/" element={<Landing/>}/>
       <Route exact path="/login" element={<LoginForm />} />
       <Route exact path="/signup" element={<Signupform />} />
+      <Route exact path="/logout" element={<Logout />} />
+  <Route exact path='/product' element={<Product />}/>
       <Route  path="*" element={<Error/>}/>
     </Routes>
     </>
